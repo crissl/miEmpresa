771fc24027d2db62cb63732c6f51a046dcad6e0f
crissl <cac.lopez@yavirac.edu.ec>
2020-07-29 10:45:04 -0500

    migraciones arregladas

--- .history/database/seeds/DatabaseSeeder_20200728000031.php
+++ .history/database/seeds/DatabaseSeeder_20200728000031.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {   
-        $this->call(UserSeeder::class);
-        $this->call(CompanySeeder::class);
-        $this->call(ProfessionalSeeder::class);
+        //$this->call(UserSeeder::class);
+        //$this->call(CompanySeeder::class);
+        //$this->call(ProfessionalSeeder::class);
         $this->call(CompanyProfessionalSeeder::class);
         
         
